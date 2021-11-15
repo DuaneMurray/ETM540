1af4284942f1a029062dafbd6d06bc37bde58398
Duane Murray <duanemurrayaz@gmail.com>
2021-11-15 12:36:11 -0800

    Modified selected attraction input field

--- Location-Maplet-Table-Shiny-App.R
+++ Location-Maplet-Table-Shiny-App.R
@@ -7,7 +7,7 @@
 library(shiny)
 library(leaflet)
 
-setwd("G:/My Drive/FALL-2021/ETM640/Project/Code/")
+setwd("G:/My Drive/FALL-2021/ETM640/Project/Code/") # SET WORKING DIR
 
 tourist_locations <- read.csv("location_data.csv") # LOAD DATA FROM FILE
 
@@ -26,7 +26,6 @@ colnames(tourist_locations) <- c("Attraction",
 # JUST A TEMP LOCATION IN CASE WE NEED TO CHANGE THE SOURCE DATA MATRIX
 refined_locations <- tourist_locations
 
-
 # USER INTERFACE SECTION DEFINITION
 ui <- fluidPage(
   
@@ -39,11 +38,13 @@ ui <- fluidPage(
     # CREATE PANEL - CAN BE SEVERAL DIFFERENT TYPES
     sidebarPanel(
       
+      # USER BUDGET
       sliderInput(inputId = "budget" ,
                   label="What is your budget?:",
                   value = 40, min=0, max=200),
     
-      selectInput("interest", "Choose an area that you are interested in:",
+      # USER CATEGORY INTERESTS - CAN SELECT MULTIPLE OPTIONS
+      selectInput("interests", "Choose the areas that you are interested in:",
                   list(`Interests` = list(
                     "Art",
                     "History",
@@ -60,12 +61,16 @@ ui <- fluidPage(
                     "Landmark",
                     "Recreational")), multiple = TRUE),
       
-      numericInput("start_time", "Start Time (24 Hour)", 1200),
-      numericInput("end_time", "End Time (24 Hour)", 2100),
-      
-      selectInput("locations", "Example Pull-Down Using Location:", 
-                  refined_locations[,1], selected = NULL, multiple = FALSE,
+      # DYNAMIC LIST FROM DATA FILE SHOWING ALL AVAILABLE ATTRACTIONS
+      selectInput("locations", "Choose Specific Locations:", 
+                  refined_locations[,1], selected = NULL, multiple = TRUE,
                   selectize = TRUE, width = NULL, size = NULL),
+      
+      # USER START TIME AVAILABLE - 24 HOUR CLOCK
+      numericInput("start_time", "Start Time (24 Hour)", 1200),
+      
+      # USER END TIME AVAILABLE - 24 HOUR CLOCK
+      numericInput("end_time", "End Time (24 Hour)", 2100),
       
       # EXAMPLE SELECTION TYPES
       numericInput("min", "Minimum", 0),
@@ -94,14 +99,20 @@ ui <- fluidPage(
 # MAIN CODE TO GENERATE DATA TO SEND TO THE USER INTERFACE SECTION
 server <- function(input, output, session) {
   
+  # DEFINE AND RUN THE OPTIMIZATION MODEL HERE <- <- <- <-
+  #
+  #
+  #
+  # SET REFINED LOCATION MATRIX VALUES AFTER PROCESSED
+  
   # OUTPUT CONTENTS OF LEAFLET MAP TO THE "mymap" AREA OF THE USER INTERFACE
   output$mymap <- renderLeaflet({
     leaflet() %>%
     addTiles() %>%
     addMarkers(lng = refined_locations[,9], 
-               lat = tourist_locations[,8], 
-               label = as.character(tourist_locations[,1]), 
-               popup = as.character(tourist_locations[,4]))
+               lat = refined_locations[,8], 
+               label = as.character(refined_locations[,1]), 
+               popup = as.character(refined_locations[,4]))
   })
 
   # OUTPUT THE CONTENTS OF THE refined_locations MATRIX IN A TABLE
